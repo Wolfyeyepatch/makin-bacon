1641ad36fd71c204a18e4800d58da4d71d9d1251
Destin <Destin.chaigneau@edu-kw1c.nl>
2025-02-09 18:36:05 +0100

    T3_REA_Oefening_2_2

---
+++ realiseren/thema 3/xampp2/htdocs/oefening2_2/index.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * User: S. Tudent
+ * Date: Date
+ * File: T3_REA_Oefening1.1.php
+ */
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title>
+        <?php
+        echo 'Realiseren instructies thema 3 en 4.';
+        ?>
+    </title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+<header>
+    <h1>
+        <?php
+        echo 'Instructies';
+        ?>
+    </h1>
+</header>
+<?php
+//Hier gaan we in het volgende hoofdstuk het menu toevoegen.
+?>
+<main id="wrapper">
+    <nav>
+        <h2>Hoofdmenu</h2>
+        <h3>Hoofdstuk 1:</h3>
+        <ul>
+            <li><a href="/REA_T3T4/Realiseren/Thema3/Hoofdstuk1/Oefening/T3_REA_Oefening1.1.php">Oefening 1.1</a></li>
+            <li><a href="/REA_T3T4/Realiseren/Thema3/Hoofdstuk1/Oefening/T3_REA_Oefening1.2.php">Oefening 1.2</a></li>
+        </ul>
+    </nav>
+    <section>
+        <h2>
+            Ajax heeft zondag voor het eerst in ruim twee jaar de koppositie in de Eredivisie veroverd. De club won zondag na een hoofdrol van Wout Weghorst met 0-2 bij Fortuna Sittard en is concurrent PSV voorbij op de ranglijst.
+        </h2>
+
+        <!-- Plaats je code / uitwerking hieronder -->
+        <img src="ajax.avif">
+        <p>
+            <?php
+            echo "Na het 1-1-gelijkspel van PSV zaterdag tegen Willem II moest Ajax winnen in Sittard om de eerste plaats over te nemen. Daar slaagde de ploeg van trainer Francesco Farioli in, maar op het spekgladde veld in Sittard ging het nogal moeizaam.
+
+Invaller Weghorst maakte het verschil voor Ajax door halverwege de tweede helft de 0-1 te maken en in de blessuretijd de 0-2 van Christian Rasmussen voor te bereiden. De 0-1 viel nadat Steven Berghuis een penalty had gemist. Daarvoor was Ajax twee keer aan een achterstand ontsnapt: Fortuna raakte de paal én de lat.
+
+Ajax staat in punten gelijk met PSV en heeft een veel minder doelsaldo dan de club uit Eindhoven (+26 om +45), maar de club uit Amsterdam mag zich tóch Eredivisie-koploper noemen. Dat is omdat Ajax een wedstrijd minder heeft gespeeld dan PSV.
+
+Voor Ajax is zo een lange periode zonder koppositie ten einde gekomen. De recordkampioen stond op 6 november 2022 voor het laatst bovenaan. Daarna viel de club ver terug door sportief en financieel wanbeleid en stond het zelfs even laatste. In die zin mag de wederopstanding van Ajax opmerkelijk worden genoemd."
+            ?>
+        </p>
+    </section>
+</main>
+</body>
+</html>
